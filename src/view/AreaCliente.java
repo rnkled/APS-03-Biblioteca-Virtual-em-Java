c40b231bda6d6533dee698ca3f7e9449ba913c9f
KenerWendel <kener_wendel@hotmail.com>
2020-05-09 20:48:55 -0300

    Revert "Merge branch 'master' of https://github.com/rnkled/aps-new"
    This reverts commit 06999569f42c4dcd285c0040922c4cbb27c066b6, reversing
    changes made to 164a19e7fd3731418dac11caf7104e41cb10b5a4.
    
    	modified:   .gitignore
    	deleted:    build/classes/icons/biblioteca.jpg
    	deleted:    build/classes/icons/logo.png
    	modified:   build/classes/view/AreaCliente.form
    	modified:   build/classes/view/AreaUsuario.form
    	modified:   build/classes/view/Login.form
    	modified:   nbproject/private/private.xml
    	deleted:    src/CustomClass/RoundedPanel.java
    	deleted:    src/icons/biblioteca.jpg
    	deleted:    src/icons/logo.png
    	modified:   src/view/AreaCliente.form
    	modified:   src/view/AreaCliente.java
    	modified:   src/view/AreaUsuario.form
    	modified:   src/view/AreaUsuario.java
    	modified:   src/view/Login.form
    	modified:   src/view/Login.java

--- src/view/AreaCliente.java
+++ src/view/AreaCliente.java
@@ -404,7 +404,7 @@ public class AreaCliente extends javax.swing.JFrame {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, 448, Short.MAX_VALUE)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
